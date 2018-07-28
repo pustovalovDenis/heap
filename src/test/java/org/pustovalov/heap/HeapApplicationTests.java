c13d5a846e5629de5b3ceca17d461aa8c0dcf844
Denis <pustovalov.denis1991@gmail.com>
2018-07-28 09:02:02 +0300

    init

---
+++ src/test/java/org/pustovalov/heap/HeapApplicationTests.java
@@ -0,0 +1,16 @@
+package org.pustovalov.heap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
